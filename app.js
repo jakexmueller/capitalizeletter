4a5ba9d864b07d131e03d70ced3249c9a7e5afe1
Jacob <jakexmueller@gmail.com>
2018-05-16 13:30:56 -0500

    removing comments

--- app.js
+++ app.js
@@ -26,43 +26,7 @@ function capitalize(wordsEntered){
 	
 	
 	
-	
-/*	
-function capitalize(wordsEntered) {
-	let phrase;
-	phrase = (wordsEntered);
-	let capPhrase = phrase.toUpperCase(wordsEntered);
-	
-	
-	
-	return capPhrase;
-	
-	
-} */
-	
-	
-	
 function getUserInput(wordsEntered){
 	let response = prompt(wordsEntered);
 	return response;
 }
-
-
-
-
-
-
-
-
-/*
-function uppercase(str)
-{
-  var array1 = str.split(' ');
-  var newarray1 = [];
-    
-  for(var x = 0; x < array1.length; x++){
-      newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
-  }
-  return newarray1.join(' ');
-}
-console.log(uppercase("the quick brown fox")); */
